6e591964150c157b6933a648be243d93e93fa406
Anton markov <amrkov5@gmail.com>
2024-08-20 00:23:01 +0300

    config:add config for lint-staged

--- api-client/.lintstagedrc.js
+++ api-client/.lintstagedrc.js
@@ -5,6 +5,11 @@ const buildEslintCommand = (filenames) =>
     .map((f) => path.relative(process.cwd(), f))
     .join(' --file ')}`;
 
+const buildPrettierCommand = (filenames) =>
+  `prettier --write ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' ')}`;
+
 module.exports = {
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand, buildPrettierCommand],
 };
