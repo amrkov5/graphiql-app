bec2254694542703716b3aa68e9acd1de987a2fd
Anton markov <amrkov5@gmail.com>
2024-08-22 11:19:28 +0300

    feat:add buttons and css for header and footer

--- api-client/src/Components/Footer/Footer.tsx
+++ api-client/src/Components/Footer/Footer.tsx
@@ -1,11 +1,21 @@
 import Link from 'next/link';
 
+import styles from './footer.module.css';
+import RSLogo from './RSLogo';
+
 export default function Footer() {
   return (
-    <footer>
-      <Link href={'https://github.com/amrkov5/graphiql-app'}>Dev repo</Link>
+    <footer className={styles.footer}>
+      <Link
+        href={'https://github.com/amrkov5/graphiql-app'}
+        className={styles.footerLink}
+      >
+        AI Team Repo
+      </Link>
       <span>2024</span>
-      <Link href="https://rs.school/">RSS pic</Link>
+      <Link href="https://rs.school/courses/reactjs">
+        <RSLogo />
+      </Link>
     </footer>
   );
 }
--- api-client/src/Components/Header/Header.tsx
+++ api-client/src/Components/Header/Header.tsx
@@ -1,12 +1,97 @@
+'use client';
+
+import {
+  auth,
+  logInWithEmailAndPassword,
+  logout,
+  registerWithEmailAndPassword,
+} from '@/firebase';
+import { onAuthStateChanged } from 'firebase/auth';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import Logo from './Logo';
+
+import styles from './header.module.css';
 
 export default function Header() {
+  const [sticky, setSticky] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setSticky(true);
+      } else {
+        setSticky(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setIsLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const onSignInMainClick = () => {
+    if (isLoggedIn) {
+      alert('go to main');
+    } else {
+      alert('go to login page');
+      logInWithEmailAndPassword('anton@mail.com', '123qweASD!');
+    }
+  };
+
+  const onSignUpOutClick = () => {
+    if (isLoggedIn) {
+      alert('logging out');
+      logout();
+    } else {
+      alert('go to register page');
+      registerWithEmailAndPassword('anton', 'anton@mail.com', '123qweASD!');
+    }
+  };
+
+  const buttons = (
+    <>
+      <button
+        onClick={onSignInMainClick}
+        className={styles.btn}
+        disabled={isLoading}
+      >
+        {`${isLoggedIn ? 'Main' : 'Sign In'}`}
+      </button>
+      <button
+        className={styles.btn}
+        onClick={onSignUpOutClick}
+        disabled={isLoading}
+      >
+        {`${isLoggedIn ? 'Sign Out' : 'Sign Up'}`}
+      </button>
+    </>
+  );
+
   return (
-    <header>
-      <Link href={'/'}>Link to /</Link>
+    <header className={`${styles.header} ${sticky ? styles.sticky : ''}`}>
+      <Link href={'/'} className={styles.headerLogo}>
+        <Logo />
+      </Link>
       <div>Language toggler</div>
-      <button>Sign In</button>
-      <button>Sign Up</button>
+      <div className={styles.btnWrapper}>{buttons}</div>
     </header>
   );
 }
