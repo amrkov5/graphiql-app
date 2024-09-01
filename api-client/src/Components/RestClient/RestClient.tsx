8beae1fbf95c3a18e87beb8588bf685bdda2bb12
veta306 <vv097099@gmail.com>
2024-09-01 17:35:15 +0300

    feat: synchronize endpoint url with query params

--- api-client/src/Components/RestClient/RestClient.tsx
+++ api-client/src/Components/RestClient/RestClient.tsx
@@ -23,10 +23,12 @@ const RestClient: React.FC<RestClientProps> = ({
 }) => {
   const searchParams = useSearchParams();
   const [method, setMethod] = useState(propMethod);
-  const [url, setUrl] = useState(propUrl ?? '');
-  const [body, setBody] = useState(propBody ?? '');
+  const [url, setUrl] = useState(propUrl ?? ''); // in base 64
+  const [body, setBody] = useState(propBody ?? ''); // in base 64
   const [queries, setQueries] = useState<KeyValuePair[]>([]);
   const [variables, setVariables] = useState<KeyValuePair[]>([]);
+
+  // sync app url
   useEffect(() => {
     const updateUrl = debounce(() => {
       let newUrl = '/' + method;
@@ -40,26 +42,60 @@ const RestClient: React.FC<RestClientProps> = ({
 
     return () => updateUrl.cancel();
   }, [method, url, body, searchParams]);
+
+  // sync endpoint url when updating queries
+  useEffect(() => {
+    const updateUrlWithQueries = debounce(() => {
+      try {
+        const parsedUrl = new URL(
+          atob(decodeURIComponent(url)),
+          window.location.origin
+        );
+        parsedUrl.search = '';
+        queries.forEach(({ key, value }) => {
+          parsedUrl.searchParams.append(key, value);
+        });
+        setUrl(
+          btoa(parsedUrl.toString().replace(window.location.origin + '/', ''))
+        );
+      } catch (error) {
+        console.error('Error updating URL with queries:', error);
+      }
+    }, 300);
+
+    updateUrlWithQueries();
+
+    return () => updateUrlWithQueries.cancel();
+  }, [queries]);
+
+  // sync queries when update endpoint url
+  useEffect(() => {
+    try {
+      const parsedUrl = new URL(
+        atob(decodeURIComponent(url)),
+        window.location.origin
+      );
+      const newQueries: KeyValuePair[] = [];
+      if (parsedUrl.href.charAt(parsedUrl.href.length - 1) === '?') {
+        newQueries.push({ id: 1000, key: '', value: '' });
+      }
+      Array.from(parsedUrl.searchParams.entries()).forEach(
+        ([key, value], index) => {
+          newQueries.push({ id: index, key, value });
+        }
+      );
+      setQueries(newQueries);
+    } catch (error) {
+      console.error('Invalid URL:', error);
+    }
+  }, [url]);
+
   return (
     <div className={styles.container}>
       <div className={styles.inputSection}>
         <MethodSelector method={method} setMethod={setMethod} />
         <EndpointInput url={url} setUrl={setUrl} />
-        <button
-          className={styles.send}
-          onClick={() => {
-            const notEmptyQueries = queries.filter(
-              (query) => query.key && query.value
-            );
-            const notEmptyVariables = variables.filter(
-              (variable) => variable.key && variable.value
-            );
-            console.log(notEmptyQueries);
-            console.log(notEmptyVariables);
-          }}
-        >
-          Send
-        </button>
+        <button className={styles.send}>Send</button>
       </div>
       <div className={styles.editors}>
         <KeyValueEditor
